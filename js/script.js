619fb0e1fde8fa93e83e0fd86065740226e98734
Hussain Islam <m.hussainul.islam@gmail.com>
2019-01-04 15:21:24 -0500

    corrected charCode to keyCode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var info = document.querySelector("#keyboardInfo");
 var key = document.querySelector("#key");
-var charcode = document.querySelector("#charcode");
+var keycode = document.querySelector("#keycode");
 var code = document.querySelector("#code");
 var altkey = document.querySelector("#altkey");
 var ctrlkey = document.querySelector("#ctrlkey");
@@ -10,10 +10,10 @@ var metakey = document.querySelector("#metakey");
 window.addEventListener("keydown", keyInfo);
 
 function keyInfo(event){
-    console.log(event);
+    //console.log(event);
     event.preventDefault();
     key.innerText = "\"" +event.key +"\"";
-    charcode.innerText = event.charCode;
+    keycode.innerText = event.keyCode;
     code.innerText = event.code;
     altkey.innerText = event.altKey;
     ctrlkey.innerText = event.ctrlKey;
